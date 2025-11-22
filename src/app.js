964ef30044561c46ea3e0eee0f6b3b1fbf8739df
Naboulibear <108967590+Naboulibear@users.noreply.github.com>
2025-11-22 20:14:57 +0000

    Project finish

--- src/app.js
+++ src/app.js
@@ -6,6 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //h
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function getRandomElement (array) { 
+let index = Math.floor(Math.random()*array.length )
+let randomElement = array[index] 
+return randomElement   
+}
+
+function getExcuse(){
+
+let randomWho = getRandomElement (who)
+let randomAction = getRandomElement (action)
+let randomWhat = getRandomElement (what)
+let randomWhen = getRandomElement (when)
+
+let excuse = randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen
+return excuse 
+
+
+
+} 
+
+let finalExcuse = document.getElementById ("Excuse")
+finalExcuse.innerHTML = getExcuse ()
+
+
+
 };
